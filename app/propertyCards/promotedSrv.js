8035dac73159c5d77a1465da847deeacf6487098
erank <eran.keinan@gmail.com>
2018-12-09 01:21:31 +0200

    update dbjason

--- app/propertyCards/promotedSrv.js
+++ app/propertyCards/promotedSrv.js
@@ -9,13 +9,13 @@ app.factory("promotedSrv", function($q, $http) {
         this.btype = plainProp.btype;
         this.type = plainProp.type;
         this.action = plainProp.action;
-        this.askingprice = plainProp.askingprice;
+        this.askingprice = plainProp.askingPrice;
         this.city = plainProp.city;
         this.street = plainProp.street;
         this.neighborhood = plainProp.neighborhood;
         this.zip = plainProp.zip;
         this.rooms = plainProp.rooms;
-        this.Size = plainProp.Size;
+        this.size = plainProp.size;
         this.floor = plainProp.floor ;
         this.floorsnums = plainProp.floorsnums ;
         this.basement = plainProp.basement ;
@@ -45,56 +45,7 @@ app.factory("promotedSrv", function($q, $http) {
     
         return async.promise;
 
-        // var promoted1 = {
-        //     "id": 1,
-        //     "promotedInd": "1",
-        //     "btype": "Business",
-        //     "type": "",
-        //     "action": "Rent",
-        //     "askingPrice": "150000",
-        //     "city": "Raanana",
-        //     "Street": "Hapnina 8",
-        //     "neighborhood": "Raanana junction",
-        //     "zip": "921921",
-        //     "rooms": "",
-        //     "Size": "5000",
-        //     "floor":"1",
-        //     "floorsnums": "3",
-        //     "basement": "",
-        //     "phone": "0503453455",
-        //     "email": "john@john.com",
-        //     "desc": "Great office area in the best location of the Sharon area where few of the most known brands are located",
-        //     "comments": "100 parking spaces are available",
-        //     "mainPicture": "https://images.haarets.co.il/image/fetch/w_857,h_482,q_auto,c_fill,f_auto/fl_any_format.preserve_transparency.progressive:none/https://www.haaretz.com/polopoly_fs/1.5561265.1514737968!/image/1018316866.jpg"
-        // }
-
-        // var promoted2 = {
-        //     "id": 1,
-        //     "promotedInd": "1",
-        //     "btype": "Business",
-        //     "type": "",
-        //     "action": "Rent",
-        //     "askingPrice": "150000",
-        //     "city": "Raanana",
-        //     "Street": "Hapnina 8",
-        //     "neighborhood": "Raanana junction",
-        //     "zip": "921921",
-        //     "rooms": "",
-        //     "Size": "5000",
-        //     "floor":"1",
-        //     "floorsnums": "3",
-        //     "basement": "",
-        //     "phone": "0503453455",
-        //     "email": "john@john.com",
-        //     "desc": "Great office area in the best location of the Sharon area where few of the most known brands are located",
-        //     "comments": "100 parking spaces are available",
-        //     "mainPicture": "https://images.haarets.co.il/image/fetch/w_857,h_482,q_auto,c_fill,f_auto/fl_any_format.preserve_transparency.progressive:none/https://www.haaretz.com/polopoly_fs/1.5561265.1514737968!/image/1018316866.jpg"
-        // }
-
-
-        // promoteds.push(promoted1);
-        // promoteds.push(promoted2);
-        // return promoteds;
+        
     }
 
     return {
--- app/propertyCards/propCardsCtrl.js
+++ app/propertyCards/propCardsCtrl.js
@@ -8,35 +8,5 @@ app.controller("propCardsCtrl", function($scope, promotedSrv, $location, $log) {
         $log.error(error);
     });
 
-    // var promoteds = [];
-
-    // var promoted = {
-    //     "id": 1,
-    //     "promotedInd": "1",
-    //     "btype": "Business",
-    //     "type": "",
-    //     "action": "Rent",
-    //     "askingPrice": "150000",
-    //     "city": "Raanana",
-    //     "Street": "Hapnina 8",
-    //     "neighborhood": "Raanana junction",
-    //     "zip": "921921",
-    //     "rooms": "",
-    //     "Size": "5000",
-    //     "floor":"1",
-    //     "floorsnums": "3",
-    //     "basement": "",
-    //     "phone": "0503453455",
-    //     "email": "john@john.com",
-    //     "desc": "Great office area in the best location of the Sharon area where few of the most known brands are located",
-    //     "comments": "100 parking spaces are available",
-    //     "mainPicture": "https://images.haarets.co.il/image/fetch/w_857,h_482,q_auto,c_fill,f_auto/fl_any_format.preserve_transparency.progressive:none/https://www.haaretz.com/polopoly_fs/1.5561265.1514737968!/image/1018316866.jpg"
-    // }
-
-    
-
-    // promoteds.push(promoted);
-    // $scope.promoteds = promoteds;
-
-    
+        
 });
