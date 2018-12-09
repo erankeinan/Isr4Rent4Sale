cb10fead9bac1ab85c1326cf0e06e7fb0daf5862
erank <eran.keinan@gmail.com>
2018-12-09 22:14:07 +0200

    Add  cookies link (bottom of page) modal

--- app/footer/footerCtrl.js
+++ app/footer/footerCtrl.js
@@ -1,5 +1,9 @@
 app.controller("footerCtrl", function($scope, promotedSrv, $location, $log) {
 
-
+    $scope.cookiesModal = function() {
+        
+        $("#cookiesModal").modal({backdrop: true});
+        $("#cookiesModal").modal({display: true});
+      };      
 
 });
