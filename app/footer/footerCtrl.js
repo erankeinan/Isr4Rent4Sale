b7e0bd3ebdabd35028451dae6f0decb269870d51
erank <eran.keinan@gmail.com>
2018-12-10 11:49:11 +0200

    Add Terms of use modal, Privacy policy modal

--- app/footer/footerCtrl.js
+++ app/footer/footerCtrl.js
@@ -4,6 +4,17 @@ app.controller("footerCtrl", function($scope, promotedSrv, $location, $log) {
         
         $("#cookiesModal").modal({backdrop: true});
         $("#cookiesModal").modal({display: true});
-      };      
+      };     
 
+    $scope.privacyModal = function() {
+    
+    $("#privacyModal").modal({backdrop: true});
+    $("#privacyModal").modal({display: true});
+    }; 
+
+    $scope.termsModal = function() {
+    
+        $("#termsModal").modal({backdrop: true});
+        $("#termsModal").modal({display: true});
+        }; 
 });
