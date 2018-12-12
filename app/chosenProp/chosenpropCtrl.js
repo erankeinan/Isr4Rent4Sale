21753e70e9be4ad58a4de8aaa659f37bf3e8c308
erank <eran.keinan@gmail.com>
2018-12-12 20:51:05 +0200

    Update dnjason - add property images

--- app/chosenProp/chosenpropCtrl.js
+++ app/chosenProp/chosenpropCtrl.js
@@ -16,26 +16,10 @@ forRentApp.controller("chosenpropCtrl", function($scope, chosenpropSrv, $locatio
     });
 
 
-    $scope.breedImages = [];
-    //alert($routeParams.breedId);
-
-   // Selected specific breed images  
-   chosenBreedSrv.getBreedImages($routeParams.breedId).then(function(breedImages) {
-        console.log("inside " + $routeParams.breedId);
-        
-        $scope.breedImages = breedImages;
-        $scope.breedName = $routeParams.breedId;
-        var prt = JSON.stringify($scope.breedImages);
-        console.log(prt);
-
-    }, function(error) {
-       $log.error(error);
-   });
- 
-   $scope.ImgEnlarge = function(breedImg) {
-    $scope.currentImg = breedImg;
-    $("#dogModal").modal({backdrop: true});
-    $("#dogModal").modal({display: true});
+   $scope.ImgEnlarge = function(imgItem) {
+    $scope.currentImg = imgItem;
+    $("#propModal").modal({backdrop: true});
+    $("#propModal").modal({display: true});
   };      
 
 
--- app/chosenProp/chosenpropSrv.js
+++ app/chosenProp/chosenpropSrv.js
@@ -1,5 +1,35 @@
 forRentApp.factory("chosenpropSrv", function($http, $log, $q) {
 
+    function getPropImages(propid) {
+      
+        var async = $q.defer();   
+        var propImages = [];
+
+        var propertImagesURL = "https://my-json-server.typicode.com/erankeinan/Isr4Rent4Sale/propertyimages?propId=" + propid;
+
+        console.log(" link: " + propertImagesURL);
+
+        $http.get(propertImagesURL).then(function(response) {
+
+            propImages = response.data.message;                                
+            var prt = JSON.stringify(response);
+            console.log(prt);
+
+            async.resolve(propImages);
+
+            }, function(error) {
+            // error
+            async.reject(error);
+            });
+            
+        return async.promise;
+
+      }//end of getPropImages
+
+      return {
+        getPropImages: getPropImages
+      };
+
 
 
 });
