bfb82a8e66bb04263d658a28bbdcf69128c57983
erank <eran.keinan@gmail.com>
2018-12-16 08:03:57 +0200

    Remove duplicate element IDs

--- app/login/loginCtrl.js
+++ app/login/loginCtrl.js
@@ -1,15 +1,15 @@
 
 forRentApp.controller("loginCtrl", function($scope, $location, user) {
     
-    $scope.email = "eran@keinan.com";
-    $scope.pwd = "1111";
+    $scope.logemail = "eran@keinan.com";
+    $scope.logpwd = "1111";
 
     $scope.invalidLogin = false;
 
     $scope.login = function() {
         $scope.invalidLogin = false;
 
-        user.login($scope.email, $scope.pwd).then(function() {
+        user.login($scope.logemail, $scope.logpwd).then(function() {
             // success login
             $location.path("/")
         }, function(error) {
